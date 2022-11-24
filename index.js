67a9076d163eed6399ef83c6e99858496652a583
Jim Bisenius <jim@curri.com>
2022-11-24 11:12:03 +0000

    type bug

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ window.onload = async function() {
     const weightToLose = params.weight - params.idealWeight;
     // days to lose weight
     const daysToLose = (params.goalDate && !params.goalDate.includes('_')) ? getDifferenceInDays(new Date(), new Date(params.goalDate)): Math.round((weightToLose/2.5) * 30.4)
-    const goalDate = (params.goalDate && !params.goalDate.includes('_')) ?? new Date(new Date.getDate() + daysToLose).toLocaleDateString()
+    const goalDate = (params.goalDate && !params.goalDate.includes('_')) ?? new Date(new Date.getDate() + Number(daysToLose)).toLocaleDateString()
     // age
     const age = params.age;
     // activeness (0-6)
